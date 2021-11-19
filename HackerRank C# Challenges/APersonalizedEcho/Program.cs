f9930dde95f258150aedf41b96f9458ebb498c57
Kyler Dennis <KylerGDennis@gmail.com>
2021-11-19 10:17:33 -0500

    Challenges from bash to C#

---
+++ HackerRank C# Challenges/APersonalizedEcho/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your personalized message:");
+
+string message = Console.ReadLine();
+
+Console.WriteLine(message);
---
+++ HackerRank C# Challenges/ComputingTheAverage/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+#Computes the average of given numbers!
+Console.WriteLine("Enter some numbers! When you are finished, type 'End'");
+List<int> numbers = new List<int>();
+
+while (true)
+{
+    string x = Console.ReadLine();
+    bool result = x.Equals("End");
+    if (result)
+    {
+        break;
+    }
+    try
+    {
+        int y = Convert.ToInt32(x);
+        numbers.Add(y);
+    }
+    catch
+    {
+        Console.WriteLine("Invalid entry! Try again!");
+    }
+}
+
+int average = 0;
+
+foreach (int i in numbers)
+{
+    average = average + i;
+}
+
+int length = numbers.Count;
+
+int finalAverage = average / length;
+
+Console.WriteLine(finalAverage);

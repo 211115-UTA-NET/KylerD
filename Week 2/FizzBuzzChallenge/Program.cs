2ef8287e5cdf63aa66150d7dd6adcd769c7d30fb
Kyler Dennis <KylerGDennis@gmail.com>
2021-12-06 17:56:54 -0500

    Week 2 folders/files

---
+++ Week 2/FizzBuzzChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fizz = Divisible by 3");
+Console.WriteLine("Buzz = Divisible by 5");
+Console.WriteLine("Fizz-Buzz = Divisible by 3 and 5");
+
+for (int i = 1; i <= 1000; i++)
+{
+    if ((i % 3 == 0) && (i % 5 != 0))
+        Console.WriteLine(i + ": Fizz");
+    else if ((i % 3 != 0) && (i % 5 == 0))
+        Console.WriteLine(i + ": Buzz");
+    else if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine(i + ": Fizz-Buzz!");
+    else
+        Console.WriteLine(i + ":");
+}
